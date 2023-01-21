7e9cc3cdf756b3288df2384899411cffecdeea92
samofwise <sam.ed.henry@gmail.com>
2023-01-21 23:57:21 +1100

    Fix

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,6 @@
+/* eslint-disable object-curly-newline */
 // https://johnnyreilly.com/2021/01/02/create-react-app-with-ts-loader-and-craco
 
-// eslint-disable-next-line no-undef
 module.exports = {
   eslint: {
     configure: {
