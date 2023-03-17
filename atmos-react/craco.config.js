9a013c6d0db4374987156059296dd698d4314dc9
samofwise <sam.ed.henry@gmail.com>
2023-03-17 20:57:32 +1100

    Fix Actions

--- atmos-react/craco.config.js
+++ atmos-react/craco.config.js
@@ -1,7 +1,17 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-multi-assign */
 /* eslint-disable object-curly-newline */
 // https://johnnyreilly.com/2021/01/02/create-react-app-with-ts-loader-and-craco
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
 
 module.exports = {
+  webpack: {
+    configure: (webpackConfig, { paths }) => {
+      paths.appBuild = webpackConfig.output.path = path.resolve('dist');
+      return webpackConfig;
+    },
+  },
   eslint: {
     configure: {
       mode: 'file',
